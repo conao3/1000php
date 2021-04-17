347b9dc235af5dbf7fdf6826b845e20b87c96c2b
Naoya Yamashita <conao3@gmail.com>
2021-04-17 21:46:28 +0900

    follow https://github.com/Nyholm/psr7#create-server-requests

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
 <?php
-echo 'Hello world';
+
+$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
+
+$serverRequest = $creator->fromGlobals();
